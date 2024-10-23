576bdf705138208d201ef5a0528b4032430016f2
Ohuka Miracle Chigozie <123806072+Miralex20@users.noreply.github.com>
2024-10-24 00:04:29 +0100

    task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 
 import redis  # type: ignore
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -17,3 +17,15 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, int, float, bytes]]:
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        return fn(value) if fn else value
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
